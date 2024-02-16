18b2b90dcc799f97230e18018652ed82990e18aa
Habeeb <skhabeebhussain91@.com>
2024-02-16 10:48:22 +0530

    Assignment Completed

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <NavBar />
+        <div className='flex flex-col items-center justify-center gap-3 p-2 '>
+          <div className='w-5/6'>
+            <button className='px-3 py-2 rounded-md bg-blue-500 text-white'>Button one</button>
+          </div>
+          <div className='w-5/6 border border-red-500 bg-red-200 p-3 rounded'>
+            <p className='text-red-600'>
+              <span className='font-medium'>Alert!</span>
+              This is awesome!
+            </p>
+          </div>
+        </div>
+        <div className='p-2 flex justify-center items-center'>
+          <Card />
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="flex justify-center items-center w-full h-12 bg-gray-200">
+            <p>Ⓒ 2024 Copyright: Kalvium-Squad61</p>
+        </div>
+    )
+}
+export default Footer; 
